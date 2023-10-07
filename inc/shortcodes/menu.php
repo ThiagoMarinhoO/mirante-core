c28607162c13aaa477e311ec6e49b96dd7c79309
Thiago Marinho <travailmarinho@gmail.com>
2023-10-07 10:25:58 -0300

    limpeza nos menus

--- inc/shortcodes/menu.php
+++ inc/shortcodes/menu.php
@@ -22,27 +22,6 @@
         <?php
     }
     add_shortcode('mega_menu', 'mega_menu');
-    
-    // Walker personalizado para personalizar o HTML do submenu
-    // class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
-    //     function start_lvl(&$output, $depth = 0, $args = null) {
-    //         // Adicione sua personalização do submenu aqui
-    //         $output .= '<ul class="sub-menu">'; // Personalize a classe da sub-menu conforme necessário
-    //     }
-    
-    //     function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
-    //         // Adicione sua personalização do item de menu aqui
-    //         $output .= '<li>';
-    //         $output .= '<a href="' . esc_url($item->url) . '">' . esc_html($item->title) . '</a>';
-    //         // Adicione a imagem à direita aqui
-    //         $output .= '<img src="sua-imagem-aqui.jpg" alt="Imagem da categoria">';
-    //     }
-    
-    //     function end_lvl(&$output, $depth = 0, $args = null) {
-    //         // Feche o submenu personalizado
-    //         $output .= '</ul>';
-    //     }
-    //  }
 
     class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
         function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
@@ -79,29 +58,3 @@
             $output .= '</ul>';
         }
     }
-    
-
-    // class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
-    //     // Override the start_lvl method to customize the sub-menu opening tag.
-    //     function start_lvl(&$output, $depth = 0, $args = null) {
-    //         $output .= '<ul class="sub-menu">';
-    //         $output .= '<img src="http://mirante.test/wp-content/uploads/2023/08/cadeira_presidente_alta_relax_com_braco_corsa_courissimo_linha_economica.jpg" alt="Imagem da categoria">';
-    //     }
-    
-    //     // Override the start_el method to customize the menu item output.
-    //     function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
-
-    //         $output .= '<li productMiniature=' . $item->url . '>';
-    //         $output .= '<a href="' . $item->url . '">' . $item->title . '</a>';
-    //     }
-    
-    //     // Override the end_el method to customize the menu item closing tag.
-    //     function end_el(&$output, $item, $depth = 0, $args = null) {
-    //         $output .= '</li>';
-    //     }
-    
-    //     // Override the end_lvl method to customize the sub-menu closing tag.
-    //     function end_lvl(&$output, $depth = 0, $args = null) {
-    //         $output .= '</ul>';
-    //     }
-    // }
